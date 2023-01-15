66b7f9370b654787c450da561608eb595693ea0e
AlexeyShelyagin <42693753+AlexeysShelyagin@users.noreply.github.com>
2023-01-15 12:44:29 +0300

    Wolfram script updated

--- Simulations/Tester.bat
+++ Simulations/Tester.bat
@@ -1,3 +1,11 @@
+<<<<<<< Updated upstream
 cd ../cmake-build-debug
 N_body_problem.exe -n test6.json -o test.bin -m euler
 N_body_problem.exe -n test6.json -o test.bin -m runge_kutta
+||||||| constructed merge base
+=======
+cd ../cmake-build-debug
+N_body_problem.exe -n test6.json -o test.bin -m euler
+N_body_problem.exe -n test6.json -o test1.bin -m runge_kutta
+pause
+>>>>>>> Stashed changes
