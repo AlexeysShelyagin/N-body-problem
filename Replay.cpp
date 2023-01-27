09056d64fe70c1a3cfe5a1658413b1eccfc14795
lendrina <elena.suboch5@gmail.com>
2023-01-27 15:32:27 +0300

    Replay fix

--- Replay.cpp
+++ Replay.cpp
@@ -13,12 +13,12 @@ Replay::Replay(std::string filename) {
     world = world_file.read_world(body_num);
     world.G = G;
 
-    frame_num = (world_file.size() / sizeof(double) - 2) / (4 * body_num + 2);
+    frame_num = (world_file.size() / sizeof(double) - 2) / (4 * body_num + 3);
     dt_list.resize(frame_num);
 
     double last_t = 0, T, dt_max = 0, end_t = 0;
     for(int i = 0; i < frame_num; i++){
-        T = world_file.read_double(sizeof(double) * (2 + (4 * body_num + 2) * i));
+        T = world_file.read_double(sizeof(double) * (2 + (4 * body_num + 3) * i));
         dt_list[i] = T - last_t;
         last_t = T;
         end_t += dt_list[i];
@@ -38,7 +38,7 @@ Replay::Replay(std::string filename) {
 
 bool Replay::load_frame(int _frame) {
     frame = _frame;
-    if(frame < frame_num) world = world_file.read_world(body_num, sizeof(double) * (2 + (4 * body_num + 2) * frame));
+    if(frame < frame_num) world = world_file.read_world(body_num, sizeof(double) * (2 + (4 * body_num + 3) * frame));
     world.dt = dt_list[frame];
 
     /*
